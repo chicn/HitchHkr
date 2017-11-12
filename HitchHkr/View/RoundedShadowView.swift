1c30330cdd98ef36887d5219cc99cad60087cf8f
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2017-11-12 15:35:45 +0900

    Add RoundedShadowView

---
+++ HitchHkr/View/RoundedShadowView.swift
@@ -0,0 +1,23 @@
+//
+//  RoundedShadowView.swift
+//  HitchHkr
+//
+//  Created by Chihiro on 2017/11/12.
+//  Copyright © 2017 chihiroshima. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowView: UIView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    func setupView() {
+        self.layer.cornerRadius = 5.0
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowColor = UIColor.darkGray.cgColor
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowOffset = CGSize(width: 0, height: 5)
+    }
+}
