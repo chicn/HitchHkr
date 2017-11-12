75492d63bd7fb4a7845e78fc5a136753b18c7f9d
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2017-11-12 17:23:07 +0900

    Add left side view and controller

---
+++ HitchHkr/Controller/LeftSidePanelVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeftSidePanelVC.swift
+//  HitchHkr
+//
+//  Created by Chihiro on 2017/11/12.
+//  Copyright © 2017 chihiroshima. All rights reserved.
+//
+
+import UIKit
+
+class LeftSidePanelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
