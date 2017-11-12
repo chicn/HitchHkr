14ad06736999f173b6ab85ac67c35d2e1f9b1de4
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2017-11-12 17:36:47 +0900

    Add ContainerVC

--- HitchHkr/Controller/ContainerVC.swift
+++ HitchHkr/Controller/ContainerVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ContainerVC: UIViewController {
 
@@ -16,20 +17,18 @@ class ContainerVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+private extension UIStoryboard {
+    class func mainStoryboard() -> UIStoryboard{
+        return UIStoryboard(name: "Main", bundle: Bundle.main)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    class func leftViewController() -> LeftSidePanelVC? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "LeftSidePanelVC") as? LeftSidePanelVC
     }
-    */
 
+    class func homeVC() -> HomeVC? {
+        return mainStoryboard().instantiateViewController(withIdentifier: "HomeVC") as? HomeVC
+    }
 }
