0dfd9c2427339875cf508e79ff6c40822abc1ada
Chihiro Shimamura <chihiros92@suou.waseda.jp>
2017-11-13 14:03:53 +0900

    Add DataService.swift

--- HitchHkr/AppDelegate.swift
+++ HitchHkr/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         window?.rootViewController = ContainerVC()
         window?.makeKeyAndVisible()
+
+        FirebaseApp.configure()
 
         return true
     }
